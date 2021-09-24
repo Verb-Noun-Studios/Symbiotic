d4ed0b78e5c113030101445255ee36dff98e10d4
christopher.foster <christopher.foster@mymail.champlain.edu>
2021-09-23 21:55:49 -0400

    firxing some errors when loading the project

--- RLFPS/Source/RLFPS/RLFPSHUD.cpp
+++ RLFPS/Source/RLFPS/RLFPSHUD.cpp
@@ -10,7 +10,7 @@
 ARLFPSHUD::ARLFPSHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/DefaultContent/FirstPerson/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
