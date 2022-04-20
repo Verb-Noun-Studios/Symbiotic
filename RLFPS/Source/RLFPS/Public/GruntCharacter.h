83220150347c28af45630f70375bd7da048522ec
thorfernbacher <thorfernbacher@gmail.com>
2022-04-20 15:05:20 -0400

    rewrite spawner in cpp with some improvements, add radius and min area settings to navmesh to prevent (some) small and unspawnable nav mesh areas

--- RLFPS/Source/RLFPS/Public/GruntCharacter.h
+++ RLFPS/Source/RLFPS/Public/GruntCharacter.h
@@ -30,6 +30,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
